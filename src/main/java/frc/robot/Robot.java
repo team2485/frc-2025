f455d8d1e6f9edde36d98fc81bbacf5c88429385
Team2485Manager <roboticsteam@francisparker.org>
2025-03-25 18:54:50 -0700

    cameras added :)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import com.pathplanner.lib.pathfinding.LocalADStar;
 import com.pathplanner.lib.pathfinding.Pathfinding;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -32,6 +33,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     Pathfinding.setPathfinder(new LocalADStar());
+    CameraServer.startAutomaticCapture();
     ctreConfigs = new CTREConfigs();
     m_robotContainer = new RobotContainer();
     AutoCommandBuilder.setRobotContainer(m_robotContainer);
